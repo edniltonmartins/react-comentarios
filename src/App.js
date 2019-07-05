866c56b09cfdb3c7789cfa30a916fe5bbc29066d
edniltonmartins <ednilton17@gmail.com>
2019-07-05 01:22:43 -0300

    Alterando projeto inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+
+class App extends Component {
+  state = {
+    comments: [
+      'Comments 1',
+      'Comments 2',
+      'Comments 3'
+    ]
+  }
+  addComments = () =>{
+    this.setState({
+      comments: [...this.state.comments, "Comentarios"]
+    })
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div>
+      <textarea></textarea>
+      <button onClick={this.addComments}>Enviar</button>
+    </div>
+    <div>
+      <div>Comments</div>
+      { this.state.comments.map( c => {
+        return <div> {c}</div>
+      })}
+      
+    </div>
     </div>
   );
+}
 }
 
 export default App;
